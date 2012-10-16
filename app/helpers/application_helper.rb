f7890f0a87ac5907c72cb7a0a37ca7a2f0d9f685
Olivier Lacan <olivier.lacan@gmail.com>
2012-10-16 17:34:58 -0400

    Allow lax spacing of code blocks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,11 +13,12 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: false)
     options = {
       autolink: true,
       no_intra_emphasis: true,
       fenced_code_blocks: true,
+      lax_spacing: true,
       lax_html_blocks: true,
       strikethrough: true,
       superscript: true
