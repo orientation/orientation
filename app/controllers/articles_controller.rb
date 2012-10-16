d4e358f731115e2a0dc73c7a2007a44ce55d8a73
Olivier Lacan <olivier.lacan@gmail.com>
2012-10-16 01:55:10 -0400

    Add simple search to index.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.search(params[:search])
   end
 
   def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ActiveRecord::Base
   belongs_to :author
+
+  def self.search(query)
+    if query
+      self.where("title ILIKE ?", "%#{query}%")
+    else
+      self.all
+    end
+  end
 end
