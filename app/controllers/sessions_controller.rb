edbcc0ede7395a69d63227fa9d6c6991fc2c1848
Olivier Lacan <olivier.lacan@gmail.com>
2012-10-24 17:20:39 -0400

    Not much, just missing a class.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
-def create
-  user = User.find_or_create_from_omniauth(auth_hash)
-  session[:user_id] = user.id
-  redirect_to root_url, notice: "Signed in!"
-end
+class ArticlesController < ApplicationController
+  def create
+    user = User.find_or_create_from_omniauth(auth_hash)
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Signed in!"
+  end
 
-def destroy
-  session[:user_id] = nil
-  redirect_to root_url, notice: "Signed out!"
-end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Signed out!"
+  end
 
-protected
+  protected
 
-def auth_hash
-  request.env['omniauth.auth']
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
