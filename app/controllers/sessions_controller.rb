cc8bfb5b451b9c355eed33959e7769f57232570c
Olivier Lacan <olivier.lacan@gmail.com>
2012-10-25 00:22:33 -0400

    Typo frenzy.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ApplicationController
+class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_omniauth(auth_hash)
     session[:user_id] = user.id
