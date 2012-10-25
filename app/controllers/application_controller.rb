7ef3b8de4301db2ab5cfa9ebb0cf1793cf563edf
Olivier Lacan <olivier.lacan@gmail.com>
2012-10-25 14:02:10 -0400

    Enough with the authentication already.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "envylabs", password: "enviouslabs"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
