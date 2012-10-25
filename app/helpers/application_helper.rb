b0378a92a201bcf9fdecaa9fb6685336f1d9a639
Olivier Lacan <olivier.lacan@gmail.com>
2012-10-25 14:04:53 -0400

    Turn off line numbers, move markdown styles where they belong.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,30 +7,8 @@ module ApplicationHelper
     def block_code(code, language)
       sha = Digest::SHA1.hexdigest(code)
       Rails.cache.fetch ["code", language, sha].join('-') do
-        add_line_numbers(Pygments.highlight(code, lexer: language))
+        Pygments.highlight(code, lexer: language)
       end
-    end
-
-    def add_line_numbers(code)
-      current_line = 0
-      code_arr = code.split("\n")
-      line_numbers = []
-      (code_arr.length - 1).times do |n|
-        current_line += 1
-        line_numbers << "<span>#{current_line}: </span>"
-      end
-      line_numbers_div = <<-HTML.strip_heredoc
-        <div class='line-numbers'>
-          #{line_numbers.join("<br />")}<br />
-        </div>
-      HTML
-
-      markdown_div = <<-HTML.strip_heredoc
-        <div class='code-block'>
-          #{line_numbers_div} 
-          #{code}
-        </div>
-      HTML
     end
   end
 
