7219acd1fdf275a10db6b7fc390435cec0b6bbec
Olivier Lacan <olivier.lacan@gmail.com>
2012-10-26 01:14:27 -0400

    A clean login route for the navigation and in case you're not logged in.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Orientation::Application.routes.draw do
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
+  get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 end
