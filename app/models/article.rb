6e282125b25920e58bc5365a5a233ade6f1770cb
Olivier Lacan <olivier.lacan@gmail.com>
2012-10-26 03:08:06 -0400

    Let's add some order in here.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
   belongs_to :author, class_name: "User"
 
   def self.search(query)
-    where("title ILIKE ?", "%#{query}%")
+    where("title ILIKE ?", "%#{query}%").order('title ASC')
   end
 end
