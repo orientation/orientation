c46131093bcb6816f75b637a6279cdbc41282289
Olivier Lacan <olivier.lacan@gmail.com>
2012-10-26 02:57:12 -0400

    What's the point of a goddamned log if you can't see why shit breaks?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Orientation::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
