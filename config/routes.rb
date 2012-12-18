da7129d97f9e4fd9f59f5780f43f45cb360dfd9d
Olivier Lacan <olivier.lacan@gmail.com>
2012-12-17 22:03:21 -0500

    Fix breakage introduced in routes by root matching of articles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Orientation::Application.routes.draw do
-  resources :articles, path: "", only: :show
-  resources :articles, except: :show
-  resources :tags
-
-  get '*id', to: 'articles#show'
-
-  root to: "articles#index"
-
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :tags
+
+  resources :articles, except: :show
+  resources :articles, path: "", only: :show
+
+  root to: "articles#index"
 end
