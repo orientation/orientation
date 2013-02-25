f0469819f15d2fde3bab798dc0a94cee1376e86f
Olivier Lacan <olivier.lacan@gmail.com>
2013-02-25 08:57:31 -0500

    Whoops, wrong attribute name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   def self.find_or_create_from_omniauth(auth)
     user = where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
     # TODO: remove when all existing users have an image
-    user.image = auth["info"]["image"] unless user.mugshot_url.present?
+    user.image = auth["info"]["image"] unless user.image.present?
     user
   end
 
