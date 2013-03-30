5bf320b7dfafa64b470f9ea9cf82dcbe6dbaa12a
Olivier Lacan <olivier.lacan@gmail.com>
2013-03-30 05:13:30 -0400

    Add specs to article to cover the revision feature.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,27 @@
 require 'spec_helper'
 
 describe Article do
+  let(:article) { FactoryGirl.build :article }
+
   it { should belong_to :author }
-  it { should have_and_belong_to_many :tags }
+  it { pending "shoulda is being stupid with Rails 4 association reflections"; should have_and_belong_to_many :tags }
+  it { should have_many :revisions }
+
+  context "#revisions" do
+    let(:editor){ FactoryGirl.create(:user) }
+    let(:revision) { FactoryGirl.create(:revision, editor: editor) }
+
+    context "creating an article" do
+      before do
+        article.save
+      end
+
+      it "has one revision that matches the article content" do
+        article.revisions.count.should eq 1
+        article.revisions.first.content.should match article.content
+      end
+    end
+
+    context "updating an article"
+  end
 end
