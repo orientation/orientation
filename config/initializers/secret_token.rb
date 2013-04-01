b3804f29687eee7eaf96880184198492e778e669
Olivier Lacan <olivier.lacan@gmail.com>
2013-03-31 20:33:19 -0400

    Update to a secret key base and extract key to ENV variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_token is kept private
 # if you're sharing your code publicly.
-Orientation::Application.config.secret_token = '044c1f731bca2a6be6b51b3d97ecb76a318dafcd7b11f50106f6648cc1ae9b437e171c088a027f0f5c78107b79999302b967eea49b860922d9bd63459c809538'
+Orientation::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
