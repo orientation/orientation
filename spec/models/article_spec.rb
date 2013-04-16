287cead752ede2678bc4fabe778a2b72c1edebde
Olivier Lacan <olivier.lacan@gmail.com>
2013-04-16 17:21:08 -0400

    Mark shoulda HABTM tests as pending.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe Article do
   it { should belong_to :author }
-  it { should have_and_belong_to_many :tags }
+  it { pending "shoulda isn't compatible with Rails 4 HABTM join tables yet."; should have_and_belong_to_many :tags }
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Tag do
-  it { should have_and_belong_to_many :articles }
+  it { pending "shoulda isn't compatible with Rails 4 HABTM join tables yet."; should have_and_belong_to_many :articles }
 end
