3241297fcf4ab123fb132ddfd73b57b4b6f8b818
Olivier Lacan <olivier.lacan@gmail.com>
2013-08-04 05:16:33 -0400

    Delegate to_s to source on AuthorDecorator.

--- app/decorators/author_decorator.rb
+++ app/decorators/author_decorator.rb
@@ -2,14 +2,18 @@ class AuthorDecorator < ApplicationDecorator
   delegate_all
   
   def email
-    source.try(:email) || "No Email"
+    source.email || "No Email"
   end
 
   def name
-    source.try(:name) || "Anonymous"
+    source.name || "Anonymous"
   end
 
   def image
-    source.try(:image) || "https://secure.gravatar.com/avatar/1c02274fedcce55a289172bfb8db25ab.jpg"
+    source.image || "https://secure.gravatar.com/avatar/1c02274fedcce55a289172bfb8db25ab.jpg"
+  end
+
+  def to_s
+    source.to_s
   end
 end
