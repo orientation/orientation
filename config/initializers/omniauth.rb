e923e499036fe16b94e24802ff42d85ef7016ddb
Olivier Lacan <olivier.lacan@gmail.com>
2013-08-05 12:31:11 -0400

    Fix OAuth prompt issue.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,6 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'],
   {
-    prompt: "none",
     image_aspect_ratio: "square",
     image_size: 40
   }
