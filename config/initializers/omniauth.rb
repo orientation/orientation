7b729e21f69455ab740d144865733fdeb3258de5
Olivier Lacan <olivier.lacan@gmail.com>
2013-09-12 12:11:45 -0400

    Ensure users are prompted to select account on Google Auth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,7 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'],
   {
+    prompt: "select_account",
     image_aspect_ratio: "square",
     image_size: 40
   }
