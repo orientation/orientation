1a66de5d04b3306551d77877605cf341448e22fa
Aimee Simone <aimeebooth@gmail.com>
2013-09-18 15:11:49 -0400

    Add default urls for test and development environments.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,4 +32,7 @@ Orientation::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Set default host
+  config.action_mailer.default_url_options = { host: 'orientation.dev' }
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Orientation::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Set default host
+  config.action_mailer.default_url_options = { host: 'orientation.dev' }
 end
