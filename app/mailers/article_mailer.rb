0f920f6ea9bfda7d5948fae31055fdc60847e9a7
Aimee Simone <aimeebooth@gmail.com>
2013-10-01 21:53:50 -0400

    ArticleMailer inherits from MandrillMailer.

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,10 +1,36 @@
-class ArticleMailer < ActionMailer::Base
+# Public: This mailer sends a stale article alert email through
+#           Mandrill instead of a normal action mailer email.
+#
+# article - a stale article (an article that has not been updated for several months)
+#
+# This email can be tested using the `.test` method:		
+#   ArticleMailer.test(:notify_author_of_staleness, email: <author.email>)
+#
+class ArticleMailer < MandrillMailer::TemplateMailer
 	default from: 'orientation@codeschool.com'
 	
 	def notify_author_of_staleness(article)
-		@article = article
-		@author = article.author
-		mail(to: @author.email, subject: "Stale Article Alert", author: @author, article: @article)
+		author = article.author
+		mandrill_mail template: 'Stale Article Alert',
+		              subject: 'Stale Article Alert',
+		              from_name: 'Code School Orientation',
+		              to: { email: author.email, name: author.name },
+		              vars: {
+		                'ARTICLE_TITLE' => article.title, 
+		                'ARTICLE_URL' => edit_article_url(article)
+		              }
 	end
+
+	test_setup_for :notify_author_of_staleness do |mailer, options|
+	  author = MandrillMailer::Mock.new({
+	  	title: 'Test', 
+	  	author: MandrillMailer::Mock.new({ 
+	  		email: options[:email] 
+	  	})
+	  })
+
+	  mailer.notify_author_of_staleness(author).deliver
+	end
+
 
 end
--- spec/mailers/article_mailer_spec.rb
+++ spec/mailers/article_mailer_spec.rb
@@ -4,23 +4,19 @@ describe ArticleMailer do
   context "notify_author_of_staleness" do
     let(:user) { FactoryGirl.create(:user, email: 'aimee@envylabs.com') }
     let(:article) { FactoryGirl.create(:article, :stale, author: user)}
-    let(:mailer) { described_class.notify_author_of_staleness(article).deliver }
+    let(:mailer) { described_class.notify_author_of_staleness(article) }
 
-    it 'has the correct subject' do
-      mailer['subject'].value.should == 'Stale Article Alert'
+    subject { mailer }
+
+    it { should send_email_to(email: user.email) }
+    it { should use_template('Stale Article Alert') }
+    it { should have_subject('Stale Article Alert') }
+    it { should be_from(email: 'orientation@codeschool.com') }
+
+    describe 'merge vars' do
+      it { should have_merge_data('ARTICLE_TITLE' => article.title) }
+      it { should have_merge_data('ARTICLE_URL' => edit_article_url(article)) }
     end
 
-    it 'gets sent to the right person' do
-      mailer['to'].value.should == user.email
-    end
-
-    it 'sets the correct "from" value' do
-      mailer['from'].value.should == 'orientation@codeschool.com'
-    end 
-
-    it 'passes the correct variables into the message' do
-      mailer.body.encoded.include?(user.name).should == true
-      mailer.body.encoded.include?(article.title).should == true
-    end
   end
 end
