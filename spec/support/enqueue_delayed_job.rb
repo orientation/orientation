ff6ae9eb0a73b2ba8eacaba2c191b68a072a0ad4
Aimee Simone <aimeebooth@gmail.com>
2013-10-03 16:43:37 -0400

    Add delayed job spec helpers.

---
+++ spec/support/enqueue_delayed_job.rb
@@ -0,0 +1,79 @@
+# -*- encoding : utf-8 -*-
+RSpec::Matchers.define(:enqueue_delayed_job) do |object, method, *args|
+  match do |block|
+    args = nil if args.empty?
+    @job_count_before = Delayed::Job.count
+    block.call
+    jobs = Delayed::Job.all
+    @matching_jobs = matching_jobs_from(jobs, object, method, args)
+    @job_count_after = jobs.size
+
+    any_jobs_enqueued? && correct_matching_job_count?
+  end
+
+  failure_message_for_should do |actual|
+    if !any_jobs_enqueued?
+      "#{message} should have created new a Delayed::Job, but didn't"
+    elsif @matching_jobs.size == 0 && jobs = matching_jobs_from(Delayed::Job.all, object, method, nil) and jobs.present?
+      "#{message} should have enqueued #{to_s}, but didn't. The following non-matching, but similar jobs (called with non-matching arguments) were enqueued: #{jobs.collect { |j| j.payload_object.inspect }.to_sentence}"
+    elsif @matching_jobs.size == 0 && jobs = matching_jobs_from(Delayed::Job.all, object, nil, nil) and jobs.present?
+      "#{message} should have enqueued #{to_s}, but didn't. The following non-matching, but similar jobs (calling non-matching methods) were enqueued: #{jobs.collect { |j| j.payload_object.inspect }.to_sentence}"
+    elsif @matching_jobs.size == 0
+      "#{message} failed to enqueue any #{to_s} jobs, or any other jobs for #{object_name}"
+    else
+      "#{message} failed to enqueue #{@count} jobs for #{to_s}"
+    end
+  end
+
+  failure_message_for_should_not do |actual|
+  end
+
+  description do
+    "delay calling #{to_s}"
+  end
+
+  chain :times do |count|
+    @count = count
+  end
+
+  define_method :message do
+    "result"
+  end
+
+  define_method :object_class do |o|
+    o.is_a?(Class) ? o : o.class
+  end
+
+  define_method :object_name do
+    if object.kind_of?(Class)
+      object.name
+    else
+      "#<%s>" % [object.class.name]
+    end
+  end
+
+  define_method :to_s do
+    if object.kind_of?(Class)
+      "%s.%s(%s)" % [object_name, method, args.collect(&:to_s).join(', ')]
+    else
+      "%s#%s(%s)" % [object_name, method, args.collect(&:to_s).join(', ')]
+    end
+  end
+
+  define_method :matching_jobs_from do |jobs, object, method, args|
+    local_jobs = jobs.select { |job| job.payload_object.kind_of?(::Delayed::PerformableMethod) }
+    local_jobs.select! { |job| object_class(job.payload_object.object) == object_class(object) } if object
+    local_jobs.select! { |job| job.payload_object.method_name.to_s =~ /^#{Regexp.escape method.to_s}(?:_without_delay)?$/ } if method
+    local_jobs.select! { |job| job.payload_object.args == args } if args
+    local_jobs
+  end
+
+  define_method :any_jobs_enqueued? do
+    @job_count_before < @job_count_after
+  end
+
+  define_method :correct_matching_job_count? do
+    (@count.nil? && @matching_jobs.size == 1) ||
+      (@count == @matching_jobs.size)
+  end
+end
---
+++ spec/support/simple_enqueue_delayed_job_matcher.rb
@@ -0,0 +1,29 @@
+# -*- encoding : utf-8 -*-
+# Public: This is a simple matcher for matching text in the handler of a delayed job,
+#         this is good enough in most cases to detect if a particuluar job was queued
+#
+# expected_job_text - Name of method or custom job handled asyncronously
+#
+# Examples
+#
+#   expect { subscription.cancel! }.to create_delayed_job_with(:MyJob)
+#
+# Returns true or error message on failure
+RSpec::Matchers.define(:create_delayed_job_with) do |expected_job_symbol|
+  match do |actual_block|
+    actual_block.call
+    Delayed::Job.any? {|job| job.handler =~ /#{expected_job_symbol.to_s}/}
+  end
+
+  failure_message_for_should do |actual|
+    "Expected the given action to create a delayed job with content: '#{expected_job_symbol.to_s}' but got: \n\n#{organize_array_output(Delayed::Job.all)}"
+  end
+
+  failure_message_for_should_not do |actual|
+    "Expected the given action to not create a delayed job with content: '#{expected_job_symbol.to_s}' but got: \n\n#{organize_array_output(Delayed::Job.all)}"
+  end
+
+  def organize_array_output(arr)
+    arr.map {|i| i.inspect}.join("\n\n")
+  end
+end
