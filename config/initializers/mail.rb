c38c60fc7968869463ae3f0e89a2220e7381bc89
Aimee Simone <aimeebooth@gmail.com>
2013-10-04 10:23:23 -0400

    Remove mandrill.yml references from mail.rb.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,22 +1,15 @@
 # -*- encoding : utf-8 -*-
 require 'yaml'
 
-begin
-  options = YAML.load_file(Rails.root.join('config', 'mandrill.yml'))[Rails.env]
+ActionMailer::Base.smtp_settings = {
+  :address   => "smtp.mandrillapp.com",
+  :port      => 587,
+  :user_name => ENV["MANDRILL_USERNAME"],
+  :password  => ENV["MANDRILL_PASSWORD"],
+  :domain    => 'heroku.com'
+}
+ActionMailer::Base.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
-    :port      => 587,
-    :user_name => ENV["MANDRILL_USERNAME"],
-    :password  => ENV["MANDRILL_PASSWORD"],
-    :domain    => 'heroku.com'
-  }
-  ActionMailer::Base.delivery_method = :smtp
-
-  MandrillMailer.configure do |config|
-    config.api_key = ENV["MANDRILL_API_KEY"]
-  end
-rescue LoadError
-  puts "The Mandrill configuration (config/mandrill.yml) is missing or malformed"
-  exit(1)
+MandrillMailer.configure do |config|
+  config.api_key = ENV["MANDRILL_API_KEY"]
 end
