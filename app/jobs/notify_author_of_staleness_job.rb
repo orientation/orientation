7f505bfeebc2bf02c52eb3b7dd36936f1c591616
Olivier Lacan <olivier.lacan@gmail.com>
2013-10-31 13:31:59 -0400

    Disable staleness notification job.

--- app/jobs/notify_author_of_staleness_job.rb
+++ app/jobs/notify_author_of_staleness_job.rb
@@ -1,8 +1,9 @@
 class NotifyAuthorOfStalenessJob < Struct.new(:articles)
 	def perform
-		articles.map do |article|
-			article.update_column(:last_notified_author_at, Date.today)
-		end
-		ArticleMailer.notify_author_of_staleness(articles).deliver
+    # FIXME: this is broken, disabled until it isn't.
+		# articles.map do |article|
+		# 	article.update_column(:last_notified_author_at, Date.today)
+		# end
+		# ArticleMailer.notify_author_of_staleness(articles).deliver
 	end
 end
