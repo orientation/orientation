cb93563351fe1ad3a8153f1e0cd2a171f80ab80e
Olivier Lacan <olivier.lacan@gmail.com>
2013-12-20 02:38:44 -0500

    Fix HABTM relationship specs.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Article do
   it { should belong_to :author }
-  it { pending "shoulda isn't compatible with Rails 4 HABTM join tables yet."; should have_and_belong_to_many :tags }
+  it { should have_and_belong_to_many :tags }
 
   context ".fresh" do
     let!(:fresh_article) { create(:article, :fresh) }
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Tag do
-  it { pending "shoulda isn't compatible with Rails 4 HABTM join tables yet."; should have_and_belong_to_many :articles }
+  it { should have_and_belong_to_many :articles }
 end
