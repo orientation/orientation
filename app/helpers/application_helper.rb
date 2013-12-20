4974a47f23d332e1267c1a9dc4e60845f3c867f7
Olivier Lacan <olivier.lacan@gmail.com>
2013-12-20 02:38:10 -0500

    Disable indented code blocks.
    Because they're meh and interfere with fenced blocks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,6 +34,7 @@ module ApplicationHelper
       autolink: true,
       no_intra_emphasis: true,
       fenced_code_blocks: true,
+      disable_indented_code_blocks: true,
       lax_spacing: true,
       lax_html_blocks: true,
       strikethrough: true,
