25d5af527116bd7e84d0db57f7d46542f9dc7489
Olivier Lacan <olivier.lacan@gmail.com>
2013-12-28 13:50:50 -0500

    Add Rails 4 migration check to RSpec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,11 @@ require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
   # ## Mock Framework
