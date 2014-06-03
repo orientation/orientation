70cbd4c76cb0c3445f8bceec32e037cb1fcb48d4
Aimee Simone <aimeebooth@gmail.com>
2014-06-03 13:31:36 -0400

    Fix tab spacing weirdness.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,95 +1,88 @@
 class ArticlesController < ApplicationController
-  before_filter :authenticate!
-  before_filter :find_article_by_params, only: [:show, :edit, :update, :destroy]
-  before_filter :decorate_article, only: [:show, :edit, :toggle_archived, :subscribe]
-  respond_to :html, :json
+	before_filter :authenticate!
+	before_filter :find_article_by_params, only: [:show, :edit, :update, :destroy]
+	before_filter :decorate_article, only: [:show, :edit, :toggle_archived, :subscribe]
+	respond_to :html, :json
 
-  def archived
-    @archived_articles = ArticleDecorator.decorate_collection(Article.archived.includes(:tags))
-  end
+	def archived
+		@archived_articles = ArticleDecorator.decorate_collection(Article.archived.includes(:tags))
+	end
 
-  def index
-    @articles = ArticleDecorator.decorate_collection(Article.current.includes(:tags).text_search(params[:search]))
-    @tags = Tag.by_article_count.take(10)
-  end
+	def index
+		@articles = ArticleDecorator.decorate_collection(Article.current.includes(:tags).text_search(params[:search]))
+		@tags = Tag.by_article_count.take(10)
+	end
 
-  def show
-    respond_with @article
-  end
+	def show
+		respond_with @article
+	end
 
-  def new
-    @article = Article.new.decorate
-  end
+	def new
+		@article = Article.new.decorate
+	end
 
-  def create
-    @article = Article.new(article_params)
-    redirect_to @article if @article.save
-  end
+	def create
+		@article = Article.new(article_params)
+		redirect_to @article if @article.save
+	end
 
-  def edit
-    @tags = @article.tags.collect{ |t| Hash["id" => t.id, "name" => t.name] }
-  end
+	def edit
+		@tags = @article.tags.collect{ |t| Hash["id" => t.id, "name" => t.name] }
+	end
 
-  def toggle_archived
-    !@article.archived? ? @article.archive! : @article.unarchive!
+	def toggle_archived
+		!@article.archived? ? @article.archive! : @article.unarchive!
 
-    flash[:notice] = "Successfully #{@article.archived? ? "archived" : "unarchived"} this article."
-    respond_with @article
-  end
+		flash[:notice] = "Successfully #{@article.archived? ? "archived" : "unarchived"} this article."
+		respond_with @article
+	end
 
-  def make_fresh
-    @article = Article.find(params[:id])
-    if @article.touch(:updated_at)
-      respond_with(@article)
-    end
-  end
+	def make_fresh
+		@article = Article.find(params[:id])
+		if @article.touch(:updated_at)
+			respond_with(@article)
+		end
+	end
 
-  def update
-    redirect_to @article if @article.update_attributes(article_params)
-  end
+	def update
+		redirect_to @article if @article.update_attributes(article_params)
+	end
 
-  def destroy
-    redirect_to articles_url if @article.destroy
-  end
+	def destroy
+		redirect_to articles_url if @article.destroy
+	end
 
-  def subscribe
-    if ArticleSubscriber.new(@article, current_user).subscribe
-      flash[:notice] = "Subscription created. You will receive weekly email notifications 
-        about this article."
-    else
-      flash[:error] = "Error creating subscription."
-    end
+	def toggle_subscription
+		if current_user.subscribed_to?(@article) 
+			# TODO: improve this query
+			current_user.article_subscriptions.where(article_id: @article.id).first.destroy
+			flash[:notice] = "Subscription destroyed. You will no longer receive 
+				weekly email notifications about this article."
+		else
+			current_user.article_subscriptions.create!(article_id: @article.id)
+			flash[:notice] = "Subscription created. You will receive weekly email notifications 
+				about this article."
+		end
 
-    respond_with @article
-  end
+		respond_with @article
+	end
 
-  def unsubscribe
-    if ArticleSubscriber.new(@article, current_user).unsubscribe
-      flash[:notice] = "Subscription destroyed. You will no longer receive 
-        weekly email notifications about this article."
-    else
-      flash[:error] = "Error destroying subscription."
-    end
+	private
 
-    respond_with @article
-  end
+	def authenticate!
+		authenticate_user! unless Rails.env.development?
+	end
 
-  private
+	def article_params
+		params.require(:article).permit(:created_at, :updated_at, :title, :content, :tag_tokens, :author_id, :editor_id, :archived_at)
+	end
 
-  def authenticate!
-    authenticate_user! unless Rails.env.development?
-  end
+	def decorate_article
+		@article = ArticleDecorator.new(find_article_by_params)
+	end
 
-  def article_params
-    params.require(:article).permit(:created_at, :updated_at, :title, :content, :tag_tokens, :author_id, :editor_id, :archived_at)
-  end
-
-  def decorate_article
-    @article = ArticleDecorator.new(find_article_by_params)
-  end
-
-  def find_article_by_params
-    @article ||= (Article.find_by_slug(params[:id]) or Article.find(params[:id]))
-  end
-  helper_method :article
+	def find_article_by_params
+		@article ||= (Article.find_by_slug(params[:id]) or Article.find(params[:id]))
+	end
+	helper_method :article
 end
