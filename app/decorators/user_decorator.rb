6c4a2a443c7ba32716b679d8dc9e8a42cb4d4127
John D. Jameson <thejohnjameson@gmail.com>
2014-08-18 22:03:58 -0400

    Implement Author inheritance from User

---
+++ app/decorators/user_decorator.rb
@@ -0,0 +1,44 @@
+class UserDecorator < ApplicationDecorator
+  delegate_all
+
+  def email
+    source.try :email || "No Email"
+  end
+
+  def email_tag
+    mail_to email, "Get in touch with #{first_name}"
+  end
+
+  def first_name
+    source.name.split(" ").first
+  end
+
+  def image
+    source.avatar.thumb.url or
+    source.try :image
+  end
+
+  def image_link(size = 40)
+    link_to image_tag(image, class: 'thumb dib', height: size, width: size), author_path(source)
+  end
+
+  def large_image
+    source.avatar.large.url
+  end
+
+  def link
+    author_path(source)
+  end
+
+  def link_tag
+    link_to name, author_path(source)
+  end
+
+  def name
+    source.try :name || "Anonymous"
+  end
+
+  def status
+    if source.active then "active" else "inactive" end
+  end
+end
