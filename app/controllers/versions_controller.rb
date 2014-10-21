b72985e3ba6f4485c42f91de25f809bc0485b35e
Olivier Lacan <hi@olivierlacan.com>
2014-10-22 01:18:48 +0200

    Display article diffs in versions

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -2,7 +2,17 @@ class VersionsController < ApplicationController
   respond_to :html, :json
 
   def show
-    @version = Version.find(params[:id])
+    @article = Article.find_by(slug: params[:article_id]).decorate
+
+    @current_version = @article.versions.where(id: params[:id]).first
+    @previous_version = @current_version.previous
+
+    if @previous_version
+      @diff = Diffy::Diff.new(@previous_version.reify.content, @current_version.reify.content).to_s
+    else
+      @diff = @current_version.reify.content
+    end
+
     respond_with @version
   end
 end
