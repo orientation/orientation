6c8e3b9790655586a4cc9d025637ea60907102ae
Thomas Meeks <thomas@thomasmeeks.com>
2014-11-02 23:24:38 -0500

    Keep sessions for a month.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_orientation_session'
+Rails.application.config.session_store :cookie_store, key: '_orientation_session', expire_after: 1.month
