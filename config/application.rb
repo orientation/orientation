6296adc597e46b87db4a3e30ca0cc26f6874164c
Olivier Lacan <hi@olivierlacan.com>
2015-03-25 06:33:32 +0100

    Use Rack::Deflater to Gzip responses
    Before: http://link.olivierlacan.com/image/3G2w2A2p1f00
    After: http://link.olivierlacan.com/image/3I3g0B30431Y

--- config/application.rb
+++ config/application.rb
@@ -54,5 +54,8 @@ module Orientation
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # https://robots.thoughtbot.com/content-compression-with-rack-deflater
+    config.middleware.use Rack::Deflater
   end
 end
