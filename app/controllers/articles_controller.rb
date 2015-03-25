64fd4a6582230254383b7e77d04c09a31f40bc55
Olivier Lacan <hi@olivierlacan.com>
2015-03-25 06:34:10 +0100

    Inject title params in new article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = Article.new.decorate
+    @article = Article.new(title: params[:title]).decorate
   end
 
   def create
