62514bdb1ca1ad69c1b7b2035d5ec0c783f20fc6
Olivier Lacan <hi@olivierlacan.com>
2015-03-28 16:11:53 +0100

    Raise when Mandrill config not set
    Ideally we would fall back to ActionMailer, but I'm too
    lazy to do that now.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,12 +4,12 @@ require 'yaml'
 ActionMailer::Base.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
   :port      => 587,
-  :user_name => ENV["MANDRILL_USERNAME"],
-  :password  => ENV["MANDRILL_PASSWORD"],
-  :domain    => ENV["MANDRILL_DOMAIN"]
+  :user_name => ENV.fetch("MANDRILL_USERNAME"),
+  :password  => ENV.fetch("MANDRILL_PASSWORD"),
+  :domain    => ENV.fetch("MANDRILL_DOMAIN")
 }
 ActionMailer::Base.delivery_method = :smtp
 
 MandrillMailer.configure do |config|
-  config.api_key = ENV["MANDRILL_API_KEY"]
+  config.api_key = ENV.fetch("MANDRILL_API_KEY")
 end
