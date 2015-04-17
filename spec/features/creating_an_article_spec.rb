1880eaec9b8037e29df57100bcfd1b4db14f064f
Olivier Lacan <hi@olivierlacan.com>
2015-04-17 04:41:21 +0200

    Add article feature spec

---
+++ spec/features/creating_an_article_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'Viewing an article' do
+  def fill(options)
+    options.each do |o|
+      fill_in o.first, with: o.second
+    end
+  end
+
+  before do
+    visit new_article_path
+    fill(
+      "article_title" => "Test",
+      "article_content" => "This is a test"
+    )
+    click_button "Create Article"
+  end
+
+  it "" do
+    expect(current_path).to_not eq(new_article_path)
+  end
+end
