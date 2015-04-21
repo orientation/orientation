074b99711a041c0f6ae87105b6672399e1a30a1d
Brandon Mathis <bemathis@gmail.com>
2015-04-21 17:35:23 -0400

    Added airbrake

---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,3 @@
+Airbrake.configure do |config|
+  config.api_key = '009e801683062a5f3648a02ba2805d18'
+end
--- config/initializers/bugsnag.rb
+++
@@ -1,5 +0,0 @@
-Bugsnag.configure do |config|
-  # the API key is set with the BUGSNAG_API_KEY environment variable
-  # config.api_key = "NOPE"
-  config.notify_release_stages = ["production"]
-end
