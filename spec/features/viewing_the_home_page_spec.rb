7259cc1088ec98782bec0434846af9937a801486
Olivier Lacan <hi@olivierlacan.com>
2015-07-30 19:11:47 -0400

    Fix bad assumption in home page feature spec
    Since the run order is random, it was possible for there to be guide
    articles in this example which caused the test to fail because when there
    are guide articles the user isn't redirected to /articles

--- spec/features/viewing_the_home_page_spec.rb
+++ spec/features/viewing_the_home_page_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe 'Viewing the home page' do
   subject(:home_visit) { visit root_path }
 
   context "when there are no guide articles" do
+    before { Article.guide.destroy_all }
+
     it "redirects to the article index" do
       expect { home_visit }.to change { current_path }.to(articles_path)
     end
