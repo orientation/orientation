f7da0e3fca61c57ec886ce5b53e84c1f3c43788a
Olivier Lacan <hi@olivierlacan.com>
2015-09-22 17:31:31 -0400

    Write real SendArticleRottenJob spec

--- spec/jobs/send_article_rotten_job_spec.rb
+++ spec/jobs/send_article_rotten_job_spec.rb
@@ -1,16 +1,15 @@
 require "rails_helper"
 
-RSpec.describe SendArticleUpdateJob do
-  let!(:article) { create(:article) }
-  let!(:user) { create(:user) }
-
-  let(:job) { described_class.new(article.id, user.id) }
-  subject(:perform_job) { job.perform }
-
+RSpec.describe SendArticleRottenJob do
   it "sends an ArticleMailer" do
-    mailer = double("ArticleMailer", deliver: true)
+    article       = create(:article)
+    reporter      = create(:user)
+    contributors  = article.contributors
+    mailer        = double("ArticleMailer", deliver: true)
 
-    expect(ArticleMailer).to receive(:send_updates_for).and_return(mailer)
-    perform_job
+    expect(ArticleMailer).to receive(:send_rotten_notification_for)
+      .with(article, contributors, reporter).and_return(mailer)
+
+    described_class.perform_now(article.id, reporter.id)
   end
 end
