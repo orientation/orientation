e4fd03aaf7ce372cc3c756979ba6487d87a3008a
Hashrocket Workstation <dev@hashrocket.com>
2015-10-01 11:58:08 -0400

    Add a comma

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -117,7 +117,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Thanks for taking the time to make someone feel good about their work."
     else
       @article.unendorse_by(current_user)
-      flash[:notice] = "Giving it then taking it right back. Ruthless, aren't we?"
+      flash[:notice] = "Giving it, then taking it right back. Ruthless, aren't we?"
     end
 
     respond_with_article_or_redirect
