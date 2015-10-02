3c24f566b74f12ecd98a25776a1acb10b6a8452f
Hashrocket Workstation <dev@hashrocket.com>
2015-10-02 09:22:17 -0400

    Add content validation and specs for crud validation

---
+++ spec/features/updating_an_article_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Updating an article" do
+  let(:article) { create(:article) }
+  before { visit edit_article_path(article) }
+
+  context "with valid parameters" do
+    before do
+      fill_in "article_title", with: "mexican food"
+      fill_in "article_content", with: "sounds good right now"
+      click_button "Update Article"
+    end
+
+    it "does not render the edit article page" do
+      expect(current_path).to_not eq(edit_article_path(article))
+      expect(page).to have_content 'Article was successfully updated.'
+    end
+  end
+
+  context "with invalid parameters" do
+    before do
+      fill_in "article_title", with: "Test"
+      fill_in "article_content", with: ""
+      click_button "Update Article"
+    end
+
+    it "renders the edit article page" do
+      expect(page).to have_content("Edit An Article")
+    end
+  end
+end
