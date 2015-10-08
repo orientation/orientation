cd8a8a3984d2789217a096367ddbfc23e9fbcd13
Olivier Lacan <hi@olivierlacan.com>
2015-10-08 17:37:58 -0400

    Fix update article feature spec

--- spec/features/updating_an_article_spec.rb
+++ spec/features/updating_an_article_spec.rb
@@ -2,34 +2,36 @@ require "rails_helper"
 
 RSpec.describe "Updating an article" do
   let(:article) { create(:article) }
+
   before { visit edit_article_path(article) }
 
   context "with valid parameters" do
     before do
       fill_in "article_title", with: "mexican food"
-      fill_in "article_content", with: "sounds good right now"
+      fill_in "article_content", with: content
       click_button "Update Article"
     end
 
-    it "does not render the edit article page" do
-      expect(current_path).to_not eq(edit_article_path(article))
-      expect(page).to have_content 'Article was successfully updated.'
+    context "and content" do
+      let(:content) { "This is a test" }
+
+      it "doesn't redirect to the edit page" do
+        expect(current_path).to_not eq(edit_article_path(article))
+        expect(page).to have_content "Article was successfully updated."
+      end
+    end
+
+    context "and no content" do
+      let(:content) { "" }
+
+      it "doesn't redirect to the edit page" do
+        expect(current_path).to_not eq(edit_article_path(article))
+        expect(page).to have_content "Article was successfully updated."
+      end
     end
   end
 
   context "with invalid parameters" do
-    context "with no content" do
-      before do
-        fill_in "article_title", with: "Test"
-        fill_in "article_content", with: ""
-        click_button "Update Article"
-      end
-
-      it "renders the edit article page" do
-        expect(page).to have_content("Edit an Article")
-      end
-    end
-
     context "with a reserved keyword" do
       before do
         fill_in "article_title", with: "javascripts"
