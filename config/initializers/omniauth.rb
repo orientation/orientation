633e52bb4efd89af8d940a5ca24243b0908a334d
Olivier Lacan <hi@olivierlacan.com>
2016-01-20 18:28:50 +0100

    Only raise in production with no email whitelist
    See https://github.com/orientation/orientation/issues/205 for the full
    conversation about this.
    
    I don't want to cripple (no sign in possible) Orientation by default for this
    setting so I want this to return `nil` if not set in every environment except
    production.
    
    This means this is now secure by default in production only and elsewhere we
    just let anyone in.
    
    It allows me to let anyone sign in to try.orientation.io

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,6 +7,16 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     image_aspect_ratio: "square",
     # we're displaying at 80 pixels, this is for high density ("Retina") displays
     image_size: 160,
-    hd: ENV.fetch("ORIENTATION_EMAIL_WHITELIST")
+    # hd means hosted domain and this option allows limiting to a particular
+    # Google Apps hosted domain. More information at:
+    #   https://developers.google.com/accounts/docs/OpenIDConnect#hd-param
+    hd: ENV.fetch("ORIENTATION_EMAIL_WHITELIST") do |element|
+      if Rails.env.production?
+        raise "ORIENTATION_EMAIL_WHITELIST is not set"
+      else
+        # we don't care if no ORIENTATION_EMAIL_WHITELIST is set in non-production environments
+        nil
+      end
+    end
   }
 end
