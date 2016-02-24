714d7c75a55ad99618206df4ed8e17d9c86ed2a1
Olivier Lacan <hi@olivierlacan.com>
2016-02-24 09:21:06 -0500

    Replace source with object in Draper

--- app/decorators/author_decorator.rb
+++ app/decorators/author_decorator.rb
@@ -2,13 +2,13 @@ class AuthorDecorator < UserDecorator
   decorates :user
 
   def email
-    if source.email && !source.private_email
+    if object.email && !object.private_email
       mail_to source.email
     end
   end
 
   def toggle_email_privacy
-    alternate_email_status = source.private_email == true ? "Public" : "Private"
-    link_to "Make Email #{alternate_email_status}", author_toggle_email_privacy_path(source), method: :put
+    alternate_email_status = object.private_email == true ? "Public" : "Private"
+    link_to "Make Email #{alternate_email_status}", author_toggle_email_privacy_path(object), method: :put
   end
 end
