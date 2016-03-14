6aae7f8de6826e4812be980d9dc9e295c902630e
Jaymes Waters <jwaters@doximity.com>
2016-03-14 15:43:13 -0500

    WIP convert to sendgrid

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,18 +1,13 @@
 # -*- encoding : utf-8 -*-
 
 if !Rails.env.test?
-  require 'yaml'
-
   ActionMailer::Base.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
-    :port      => 587,
-    :user_name => ENV.fetch("MANDRILL_USERNAME"),
-    :password  => ENV.fetch("MANDRILL_API_KEY"),
-    :domain    => ENV.fetch("MANDRILL_DOMAIN")
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV.fetch('SENDGRID_USERNAME'),
+    :password       => ENV.fetch('SENDGRID_PASSWORD'),
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
-  ActionMailer::Base.delivery_method = :smtp
-
-  MandrillMailer.configure do |config|
-    config.api_key = ENV.fetch("MANDRILL_API_KEY")
-  end
 end
