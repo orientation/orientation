395a53bc7b80a186a0d0a60cba4abe7a12c9c461
Olivier Lacan <hi@olivierlacan.com>
2016-04-28 19:01:48 -0400

    Don't notify the endorser about their endorsement
    Because that's a little silly.

--- app/jobs/send_article_endorsement_job.rb
+++ app/jobs/send_article_endorsement_job.rb
@@ -2,10 +2,14 @@ class SendArticleEndorsementJob < ActiveJob::Base
   queue_as :default
 
   def perform(endorsement_id)
+
     endorsement  = ArticleEndorsement.find(endorsement_id)
     article      = endorsement.article
     endorser     = endorsement.user
-    contributors = article.contributors
+
+    contributors = article.contributors.reject do |contributor|
+      contributor[:email] == endorser.email
+    end
 
     ArticleMailer.send_endorsement_notification_for(article, contributors, endorser).deliver
   end
