c6c742f0623e187f51cd83ae88911b8dcebae621
Olivier Lacan <hi@olivierlacan.com>
2016-05-03 18:38:43 -0500

    Use env var credentials over shared credentials

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,13 +2,5 @@ require 'aws-sdk'
 
 Aws.config.update({
   region: 'us-east-1',
-  credentials: Aws::SharedCredentials.new({
-    # the `path` option is inferred so make sure you have a ~/.aws/credentials
-    # file that contains the following:
-    #   [orientation]
-    #   aws_access_key_id = YOURACCESSKEYID
-    #   aws_secret_access_key = YOURSECRETKEYID
-    #
-    profile_name: "orientation"
-  })
+  credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
 })
