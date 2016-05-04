84ee43b60eb079d9d23cfa7116a5fdd19b4e8529
Olivier Lacan <hi@olivierlacan.com>
2016-05-04 01:19:26 -0500

    Add missing require for action cable

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
