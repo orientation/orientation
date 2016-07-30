b4a45b40d2f8cb541af1865c96021c71ccea74d7
Timothy Lim <tim.lim@intercom.io>
2016-07-30 23:10:17 +0800

    Allow editing and deletion of a tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,10 @@
 class TagsController < ApplicationController
+  before_action :find_tag_by_params, only: [
+    :show,
+    :edit,
+    :update,
+    :destroy
+  ]
   respond_to :json, :html
 
   def index
@@ -17,18 +23,31 @@ class TagsController < ApplicationController
     respond_with @tag = Tag.create(tag_params)
   end
 
-  def show
-    @tag = find_tag_by_params
+  def destroy
+    redirect_to tags_url if @tag.destroy
   end
 
-  def edit
-    @tag = find_tag_by_params
+  def update
+    if @tag.update_attributes(tag_params)
+      flash[:notice] = "Tag was successfully updated."
+    else
+      flash[:error] = error_message(@tag)
+    end
+    respond_with @tag
   end
 
   private
 
+  def error_message(tag)
+    if tag.errors.messages.key?(:name)
+      "#{tag.name} #{tag.errors.messages[:name].first}."
+    else
+      "Tag could not be #{params[:action]}d."
+    end
+  end
+
   def find_tag_by_params
-    Tag.friendly.find(params[:id])
+    @tag = Tag.friendly.find(params[:id])
   end
 
   def tag_params
