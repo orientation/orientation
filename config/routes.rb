a33fc460ddb0d4789a4adff97f8e873cea47cb83
Olivier Lacan <hi@olivierlacan.com>
2016-10-07 21:38:09 +0200

    Namespace auth routes
    Just a bit of clean up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-
-  get 'auth/:provider/callback', to: 'sessions#create', as: :oauth_callback
-  get 'auth/failure', to: redirect('/')
+  namespace :auth do
+    get ":provider/callback", to: "sessions#create", as: :oauth_callback
+    get :failure, to: redirect("/")
+  end
+ 
   get :sign_in, to: "sessions#new", as: :sign_in
   get :sign_out, to: "sessions#destroy", as: :sign_out
 
