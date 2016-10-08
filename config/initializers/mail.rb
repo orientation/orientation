30f06cbaa5baa3a588e549ab87cbbc792b0e9cde
Olivier Lacan <hi@olivierlacan.com>
2016-10-08 19:11:16 +0200

    Skip Mandrill config if env vars aren’t set

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,18 +1,26 @@
-# -*- encoding : utf-8 -*-
-
 if !Rails.env.test?
-  require 'yaml'
+  if mandrill_configured?
+    require 'yaml'
 
-  ActionMailer::Base.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
-    :port      => 587,
-    :user_name => ENV.fetch("MANDRILL_USERNAME"),
-    :password  => ENV.fetch("MANDRILL_API_KEY"),
-    :domain    => ENV.fetch("MANDRILL_DOMAIN")
-  }
-  ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      address:   "smtp.mandrillapp.com",
+      port:      587,
+      user_name: ENV["MANDRILL_USERNAME"],
+      password:  ENV["MANDRILL_API_KEY"],
+      domain:    ENV["MANDRILL_DOMAIN"]
+    }
+    ActionMailer::Base.delivery_method = :smtp
 
-  MandrillMailer.configure do |config|
-    config.api_key = ENV.fetch("MANDRILL_API_KEY")
+    MandrillMailer.configure do |config|
+      config.api_key = ENV["MANDRILL_API_KEY"]
+    end
+  else
+    Rails.logger.info { "Mandrill environment variables not set. Transactional emails will not be sent."}
   end
+end
+
+def mandrill_configured?
+  ENV["MANDRILL_USERNAME"].present? && 
+  ENV["MANDRILL_API_KEY"].present? &&
+  ENV["MANDRILL_DOMAIN"].present?
 end
