a8f9458bca4efc56c842dbc4e6ca2ea0f04c0e02
Olivier Lacan <hi@olivierlacan.com>
2016-10-08 18:50:05 +0200

    Format mailer configuration consistently

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,9 +62,10 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
+
   config.mandrill_mailer.default_url_options =
-  config.action_mailer.default_url_options =
-  { host: ENV.fetch("ORIENTATION_DOMAIN") }
+    config.action_mailer.default_url_options =
+      { host: ENV.fetch("ORIENTATION_DOMAIN") }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
