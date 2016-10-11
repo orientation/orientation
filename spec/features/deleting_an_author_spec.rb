226aef41bc534f58a70d6a6d862947edfec2f1d4
Julio Monteiro <julio@monteiro.eti.br>
2016-10-11 18:29:21 -0300

    Separating into different 'it's

--- spec/features/deleting_an_author_spec.rb
+++ spec/features/deleting_an_author_spec.rb
@@ -27,18 +27,38 @@ RSpec.describe 'deleting an author' do
     let(:edit) { create(:article, editor: author) }
     let(:rot_report) { create(:article, rot_reporter: author) }
 
-    it "re-assigns the author to be logged in user" do
+    it "re-assigns articles to a new user" do
       with_modified_env(ADMINISTRATORS: "42,#{logged_in_user.id},10101") do
         visit author_path(author.id)
 
         expect do
-          expect do
-            expect do
-              select logged_in_user.name, from: 'replacement_author_id', wait: 3
-              click_link_or_button 'Delete Author'
-              expect(current_path).to eq(authors_path)
-            end.to change { article.reload.author }.from(author).to(logged_in_user)
-          end.to change { edit.reload.editor }.from(author).to(logged_in_user)
+          select logged_in_user.name, from: 'replacement_author_id', wait: 3
+          click_link_or_button 'Delete Author'
+          expect(current_path).to eq(authors_path)
+        end.to change { article.reload.author }.from(author).to(logged_in_user)
+      end
+    end
+
+    it "re-assigns edits to a new user" do
+      with_modified_env(ADMINISTRATORS: "42,#{logged_in_user.id},10101") do
+        visit author_path(author.id)
+
+        expect do
+          select logged_in_user.name, from: 'replacement_author_id', wait: 3
+          click_link_or_button 'Delete Author'
+          expect(current_path).to eq(authors_path)
+        end.to change { edit.reload.editor }.from(author).to(logged_in_user)
+      end
+    end
+
+    it "re-assigns rot reports to a new user" do
+      with_modified_env(ADMINISTRATORS: "42,#{logged_in_user.id},10101") do
+        visit author_path(author.id)
+
+        expect do
+          select logged_in_user.name, from: 'replacement_author_id', wait: 3
+          click_link_or_button 'Delete Author'
+          expect(current_path).to eq(authors_path)
         end.to change { rot_report.reload.rot_reporter }.from(author).to(logged_in_user)
       end
     end
