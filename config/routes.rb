9a0a0882a73a7f0da415d70f92a6006c7d0f49d9
Jason Seifer <jseifer@doximity.com>
2016-10-28 18:41:04 -0400

    Add versioning and restore

--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,14 @@ Rails.application.routes.draw do
 
   resources :articles do
     resources :update_requests
+    resources :article_versions, only: [:index, :show, :update]
     collection do
       get :fresh
       get :stale
       get :rotten
       get :archived
       get :popular
+      get :search
     end
     member do
       put :toggle_subscription
