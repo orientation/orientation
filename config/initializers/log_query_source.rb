68a85586df8ee424c879b2522f910acad6f25cfa
Olivier Lacan <hi@olivierlacan.com>
2017-04-08 03:34:09 +0200

    Log the source of SQL queries in test/dev
    I really need to finish my Rails PR for this feature:
    https://github.com/rails/rails/pull/26815

---
+++ config/initializers/log_query_source.rb
@@ -0,0 +1,19 @@
+module LogQuerySource
+  def debug(*args, &block)
+    return unless super
+
+    backtrace = Rails.backtrace_cleaner.clean caller
+
+    relevant_caller_line = backtrace.detect do |caller_line|
+      !caller_line.include?('/initializers/')
+    end
+
+    if relevant_caller_line
+      logger.debug("  ↳ #{ relevant_caller_line.sub("#{ Rails.root }/", '') }")
+    end
+  end
+end
+
+if Rails.env.development? || Rails.env.test?
+  ActiveRecord::LogSubscriber.send(:prepend, LogQuerySource)
+end
