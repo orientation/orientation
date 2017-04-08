fd121ab3d5942aedf0ed025024adf725d42ed4a3
Olivier Lacan <hi@olivierlacan.com>
2017-04-08 08:07:24 +0200

    Ignore authentication & layout for OpenSearch

--- app/controllers/open_search/descriptions_controller.rb
+++ app/controllers/open_search/descriptions_controller.rb
@@ -1,6 +1,11 @@
 module OpenSearch
   class DescriptionsController < ApplicationController
+    layout false
+
     respond_to :osd_xml
+
+    skip_before_action :authenticate_user!
+
     # Serves the OpenSearch Description document so that you can use the omnibox
     # and similar to search on Orientation.
     def show
