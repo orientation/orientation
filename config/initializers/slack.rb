175f180dcec5bb11c4e8993363fd6cc3e799df33
Olivier Lacan <hi@olivierlacan.com>
2017-06-30 05:15:52 +0200

    Update slack-notifier to 2.2.2
    This may break things, I have to test this in production.

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,7 +1,8 @@
 SLACK_CONFIG = if ENV["SLACK_WEBHOOK_URL"]
-  Slack::Notifier.new ENV["SLACK_WEBHOOK_URL"],
-  username: ENV['DEFAULT_FROM_NAME'] || 'Orientation',
-  icon_emoji: ":book:"
+  notifier = Slack::Notifier.new ENV["SLACK_WEBHOOK_URL"] do
+    defaults  username: ENV['DEFAULT_FROM_NAME'] || 'Orientation',
+              icon_emoji: ":book:"
+  end
 else
   nil
 end
