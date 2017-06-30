7936a947331dc32beca82cb95fbe2296ab633591
Olivier Lacan <hi@olivierlacan.com>
2017-06-30 05:32:15 +0200

    Do not allow unknown asset fallback

--- config/initializers/new_framework_defaults_5_1.rb
+++ config/initializers/new_framework_defaults_5_1.rb
@@ -11,4 +11,4 @@ Rails.application.config.action_view.form_with_generates_remote_forms = false
 
 # Unknown asset fallback will return the path passed in when the given
 # asset is not present in the asset pipeline.
-# Rails.application.config.assets.unknown_asset_fallback = false
+Rails.application.config.assets.unknown_asset_fallback = false
