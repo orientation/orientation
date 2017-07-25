e1e113554264e6817dcbff1ff7cb41418b563bc2
Olivier Lacan <hi@olivierlacan.com>
2017-07-25 04:30:49 +0200

    Keep backtraces on job errors

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,7 @@
 require 'sidekiq/web'
 
+Sidekiq.default_worker_options = { 'backtrace' => true }
+
 if Rails.env.production?
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
