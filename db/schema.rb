5b36aa9c514dc128100ef3ee0723075297cef334
Olivier Lacan <hi@olivierlacan.com>
2018-10-06 00:24:36 -0400

    Upgrade schema.rb with serial id parameters

--- db/schema.rb
+++ db/schema.rb
@@ -17,108 +17,108 @@ ActiveRecord::Schema.define(version: 20170224025153) do
   enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
 
-  create_table "article_endorsements", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "article_id"
+  create_table "article_endorsements", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "article_subscriptions", force: :cascade do |t|
-    t.integer  "article_id"
-    t.integer  "user_id"
+  create_table "article_subscriptions", id: :serial, force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "article_views", force: :cascade do |t|
-    t.integer  "article_id",             null: false
-    t.integer  "user_id",                null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "count",      default: 1
-    t.index ["article_id", "user_id"], name: "index_article_views_on_article_id_and_user_id", unique: true, using: :btree
-    t.index ["article_id"], name: "index_article_views_on_article_id", using: :btree
-    t.index ["user_id"], name: "index_article_views_on_user_id", using: :btree
+  create_table "article_views", id: :serial, force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "count", default: 1
+    t.index ["article_id", "user_id"], name: "index_article_views_on_article_id_and_user_id", unique: true
+    t.index ["article_id"], name: "index_article_views_on_article_id"
+    t.index ["user_id"], name: "index_article_views_on_user_id"
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.integer  "author_id"
+  create_table "articles", id: :serial, force: :cascade do |t|
+    t.string "title", limit: 255
+    t.text "content"
+    t.integer "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
-    t.integer  "editor_id"
+    t.string "slug", limit: 255
+    t.integer "editor_id"
     t.datetime "last_notified_author_at"
     t.datetime "archived_at"
     t.datetime "outdated_at"
-    t.integer  "tags_count",               default: 0,     null: false
-    t.boolean  "guide",                    default: false
-    t.integer  "subscriptions_count",      default: 0
-    t.integer  "endorsements_count",       default: 0
-    t.integer  "visits",                   default: 0,     null: false
-    t.integer  "outdatedness_reporter_id"
+    t.integer "tags_count", default: 0, null: false
+    t.boolean "guide", default: false
+    t.integer "subscriptions_count", default: 0
+    t.integer "endorsements_count", default: 0
+    t.integer "visits", default: 0, null: false
+    t.integer "outdatedness_reporter_id"
     t.index "to_tsvector('english'::regconfig, (title)::text)", name: "articles_title", using: :gin
     t.index "to_tsvector('english'::regconfig, content)", name: "articles_content", using: :gin
-    t.index ["archived_at"], name: "index_articles_on_archived_at", using: :btree
-    t.index ["slug"], name: "index_articles_on_slug", unique: true, using: :btree
+    t.index ["archived_at"], name: "index_articles_on_archived_at"
+    t.index ["slug"], name: "index_articles_on_slug", unique: true
   end
 
-  create_table "articles_tags", force: :cascade do |t|
+  create_table "articles_tags", id: :serial, force: :cascade do |t|
     t.integer "article_id"
     t.integer "tag_id"
-    t.index ["article_id", "tag_id"], name: "index_articles_tags_on_article_id_and_tag_id", using: :btree
+    t.index ["article_id", "tag_id"], name: "index_articles_tags_on_article_id_and_tag_id"
   end
 
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
+  create_table "delayed_jobs", id: :serial, force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
+    t.string "locked_by", limit: 255
+    t.string "queue", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
     t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "slug"
-    t.integer  "articles_count", default: 0, null: false
-    t.index ["slug"], name: "index_tags_on_slug", unique: true, using: :btree
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "email"
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
-    t.string   "avatar"
-    t.boolean  "active",      default: true
-    t.text     "shtick"
-    t.json     "preferences"
+    t.string "slug", limit: 255
+    t.integer "articles_count", default: 0, null: false
+    t.index ["slug"], name: "index_tags_on_slug", unique: true
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "provider", limit: 255
+    t.string "uid", limit: 255
+    t.string "name", limit: 255
+    t.string "email", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "image", limit: 255
+    t.string "avatar", limit: 255
+    t.boolean "active", default: true
+    t.text "shtick"
+    t.json "preferences"
   end
 
 end
