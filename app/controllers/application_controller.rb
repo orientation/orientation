dc133cd1e743c7fa0e502c5f5c7948b5723a2e06
Olivier Lacan <hi@olivierlacan.com>
2018-10-05 23:34:16 -0400

    Remove now defaulted protect_from_forgery call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
   before_action :authenticate_user!
   before_action :warn_about_email_whitelist
 
