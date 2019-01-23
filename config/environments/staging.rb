1e472d0021c131efea5b638e9dbdd92750447e13
Olivier Lacan <hi@olivierlacan.com>
2019-01-24 00:23:17 +0100

    Add missing static file server for staging
    It's in prod, should be in staging (demo site)

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,6 +14,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
