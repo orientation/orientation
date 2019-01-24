6b86933b0407583b260bc59648b7638e911deab2
Olivier Lacan <hi@olivierlacan.com>
2019-01-24 19:03:31 +0100

    Use realistic avatars for realisticness
    It’s a word if you want it to be

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     provider { Faker::Company.name.parameterize.underscore }
     uid { "#{Faker::Number.digit}" }
     name { Faker::Name.name }
-    sequence(:email) { |n| "email#{n}@hanso.dk" }
+    sequence(:email) { |n| "example-email#{n}@orientation.io" }
+    avatar { "http://i.pravatar.cc/160?u=#{email}" }
   end
 end
